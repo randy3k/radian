8bfcf7b6793512bcf128c8211b0051eaa30384f9
Randy Lai <randy.cs.lai@gmail.com>
2017-04-02 05:45:19 -0400

    add long description

--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
 from setuptools import setup
+import os
+
+long_description = open(
+    os.path.join(
+        os.path.dirname(__file__),
+        'README.md'
+    )
+).read()
 
 setup(
     name='role',
@@ -6,6 +14,7 @@ setup(
     version='0.0.1',
     url='https://github.com/randy3k/role',
     description='R REPL build on top of prompt_toolkit',
+    long_description=long_description,
     packages=["role"],
     install_requires=[
         'prompt_toolkit>=1.0.14,<2.0.0',
