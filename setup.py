0b97ca23fac3bc24b85c07dd5807b272858e4d5e
Randy Lai <randy.cs.lai@gmail.com>
2017-08-15 04:08:44 -0400

    tag 0.0.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except (IOError, ImportError):
 setup(
     name='rice',
     author='Randy Lai',
-    version='0.0.3-dev',
+    version='0.0.1',
     url='https://github.com/randy3k/rice',
     description='R REPL build on top of prompt_toolkit',
     long_description=long_description,
