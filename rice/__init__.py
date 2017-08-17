3e25615c7efd114727983722308a4cb36079a597
Randy Lai <randy.cs.lai@gmail.com>
2017-08-17 14:46:24 -0400

    bump to 0.0.6

--- rice/__init__.py
+++ rice/__init__.py
@@ -1,1 +1,3 @@
 from .entry_points import main
+
+__version__ = '0.0.6'
--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+import os
+import re
 from setuptools import setup, find_packages
 
 try:
@@ -7,10 +9,19 @@ except:
     long_description = ''
 
 
+def get_version(package):
+    """
+    Return package version as listed in `__version__` in `__init__.py`.
+    """
+    path = os.path.join(os.path.dirname(__file__), package, '__init__.py')
+    with open(path, 'rb') as f:
+        init_py = f.read().decode('utf-8')
+    return re.search("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
+
 setup(
     name='rice',
     author='Randy Lai',
-    version='0.0.5',
+    version=get_version("rice"),
     url='https://github.com/randy3k/rice',
     description='R CLI built on top of prompt_toolkit',
     long_description=long_description,
