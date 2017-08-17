b2711bf4cde6910f2bae7e47550bd814c0d5a727
Randy Lai <randy.cs.lai@gmail.com>
2017-08-17 15:05:17 -0400

    add help menu

--- rice/__init__.py
+++ rice/__init__.py
@@ -1,3 +1,19 @@
-from .entry_points import main
+from __future__ import unicode_literals
+import optparse
+from . import deps
+from .application import RiceApplication
 
 __version__ = '0.0.6'
+
+
+def main():
+    parser = optparse.OptionParser("usage: rice")
+    parser.add_option("-v", "--version", action="store_true", dest="version", help="get version")
+
+    options, args = parser.parse_args()
+
+    if options.version:
+        print("version {}".format(__version__))
+        return
+
+    RiceApplication().run()
--- rice/entry_points.py
+++
@@ -1,7 +0,0 @@
-from __future__ import unicode_literals
-from . import deps
-from .application import RiceApplication
-
-
-def main():
-    RiceApplication().run()
