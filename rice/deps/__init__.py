d3c53c911cc8a3a1fbb19091c6990d75f61db4ea
Randy Lai <randy.cs.lai@gmail.com>
2017-09-04 01:14:54 -0400

    add a note about prompt_toolkit

--- rice/deps/__init__.py
+++ rice/deps/__init__.py
@@ -1,5 +1,8 @@
 import os
 import sys
 
+# this directory would not be needed after prompt_toolkit 2.0 is released.
+
+
 deps_path = os.path.join(os.path.dirname(__file__))
 sys.path.insert(1, deps_path)
