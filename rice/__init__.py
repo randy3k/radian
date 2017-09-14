c607ae8570092ad5ace810627fb84f327ced4ce2
Randy Lai <randy.cs.lai@gmail.com>
2017-09-14 13:38:25 -0400

    acccept --vanilla etc
    close #10

--- rice/__init__.py
+++ rice/__init__.py
@@ -11,6 +11,10 @@ __version__ = '0.0.13'
 def main():
     parser = optparse.OptionParser("usage: rice")
     parser.add_option("-v", "--version", action="store_true", dest="version", help="get version")
+    parser.add_option("--no-environ", action="store_true", dest="no_environ", help="Don't read the site and user environment files")
+    parser.add_option("--no-site-file", action="store_true", dest="no_site_file", help="Don't read the site-wide Rprofile")
+    parser.add_option("--no-init-file", action="store_true", dest="no_init_file", help="Don't read the user R profile")
+    parser.add_option("--vanilla", action="store_true", dest="vanilla", help="Combine --no-environ --no-site-file --no-init-file")
 
     options, args = parser.parse_args()
 
@@ -19,5 +23,24 @@ def main():
         return
 
     os.environ["RICE_VERSION"] = __version__
+
+    no_environ = options.no_environ
+    no_site_file = options.no_site_file
+    no_init_file = options.no_init_file
+
+    if options.vanilla:
+        no_environ = True
+        no_site_file = True
+        no_init_file = True
+
+    if no_environ:
+        os.environ["R_ENVIRON"] = ""
+        os.environ["R_ENVIRON_USER"] = ""
+
+    if no_site_file:
+        os.environ["R_PROFILE"] = ""
+
+    if no_init_file:
+        os.environ["R_PROFILE_USER"] = ""
 
     RiceApplication().run()
