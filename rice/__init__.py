be7a1934b4a61b049a5354825ba48c85d766475f
Randy Lai <randy.cs.lai@gmail.com>
2017-09-22 00:02:16 -0400

    bump to 0.0.25

--- rice/__init__.py
+++ rice/__init__.py
@@ -5,8 +5,7 @@ import os
 from . import deps
 from .riceapp import RiceApplication
 
-__version__ = '0.0.24'
-
+__version__ = '0.0.25'
 
 def main():
     parser = optparse.OptionParser("usage: rice")
