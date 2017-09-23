ca2b7573a63d0bf08ee8eec109d9e3ad50eab477
Randy Lai <randy.cs.lai@gmail.com>
2017-09-23 00:19:48 -0400

    from __future__ import unicode_literals

--- rice/modalbuffer.py
+++ rice/modalbuffer.py
@@ -1,3 +1,4 @@
+from __future__ import unicode_literals
 from prompt_toolkit.buffer import Buffer
 from prompt_toolkit.application.current import get_app
 
--- rice/modalhistory.py
+++ rice/modalhistory.py
@@ -1,3 +1,4 @@
+from __future__ import unicode_literals
 import datetime
 import os
 from prompt_toolkit.history import History
