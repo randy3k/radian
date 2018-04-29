038729d2815cdd98f4c2f5cece0d362499248432
Randy Lai <randy.cs.lai@gmail.com>
2018-04-28 23:21:25 -0400

    redraw layout when stdout.flush() returns an error
    it is needed to allow redirecting stdout

--- rtichoke/rtichokeprompt.py
+++ rtichoke/rtichokeprompt.py
@@ -1,11 +1,12 @@
 import os
 import sys
+import errno
 
 from prompt_toolkit.application.current import get_app
 from prompt_toolkit.completion import DynamicCompleter
 from prompt_toolkit.lexers import DynamicLexer, PygmentsLexer
 from prompt_toolkit.output import ColorDepth
-from prompt_toolkit.utils import is_windows
+from prompt_toolkit.utils import is_windows, get_term_environment_variable
 
 from pygments.lexers.r import SLexer
 
@@ -18,11 +19,46 @@ from .completion import RCompleter, SmartPathCompleter
 
 if not is_windows():
     from prompt_toolkit.input.vt100 import Vt100Input
+    from prompt_toolkit.output.vt100 import Vt100_Output
 
     class CustomVt100Input(Vt100Input):
         @property
         def responds_to_cpr(self):
             return False
+
+    class CustomVt100Output(Vt100_Output):
+
+        def flush(self):
+            # it is needed when the stdout was redirected
+            # see https://github.com/Non-Contradiction/JuliaCall/issues/39
+            try:
+                if self._buffer:
+                    data = ''.join(self._buffer)
+                    if self.write_binary:
+                        if hasattr(self.stdout, 'buffer'):
+                            out = self.stdout.buffer  # Py3.
+                        else:
+                            out = self.stdout
+                        out.write(data.encode(self.stdout.encoding or 'utf-8', 'replace'))
+                    else:
+                        self.stdout.write(data)
+                    self._buffer = []
+            except IOError as e:
+                if e.args and e.args[0] == errno.EINTR:
+                    pass
+                elif e.args and e.args[0] == 0:
+                    pass
+                else:
+                    raise
+            try:
+                self.stdout.flush()
+            except IOError as e:
+                if e.args and e.args[0] == errno.EAGAIN:
+                    app = get_app()
+                    app.renderer.render(app, app.layout)
+                    pass
+                else:
+                    raise
 
 
 def create_rtichoke_prompt(options, history_file, inputhook):
@@ -67,6 +103,11 @@ def create_rtichoke_prompt(options, history_file, inputhook):
             shell_cmd.run_shell_command(buff.text)
             buff.reset()
 
+    if is_windows():
+        output = None
+    else:
+        output = CustomVt100Output.from_pty(sys.stdout, term=get_term_environment_variable())
+
     mp = ModalPrompt(
         lexer=DynamicLexer(get_lexer),
         completer=DynamicCompleter(get_completer),
@@ -76,6 +117,7 @@ def create_rtichoke_prompt(options, history_file, inputhook):
         tempfile_suffix=".R",
         accept=accept,
         input=CustomVt100Input(sys.stdin) if not is_windows() else None,
+        output=output,
         inputhook=inputhook
     )
 
