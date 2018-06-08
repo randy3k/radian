0239cd0c35ba46a69448a885f6d5904973550747
Randy Lai <randy.cs.lai@gmail.com>
2018-06-08 17:02:23 -0400

    add test for rtichoke --version

--- tests/test_startup.py
+++ tests/test_startup.py
@@ -4,8 +4,14 @@ from helpers import assert_equal, assert_startswith, screen_process
 
 
 def test_startup():
+    rtichoke_command = [sys.executable, "-m", "rtichoke"]
 
-    with screen_process([sys.executable, "-m", "rtichoke"]) as screen:
+    with screen_process(rtichoke_command) as screen:
         assert_startswith(lambda: screen.display[0], "R ")
         assert_equal(lambda: (screen.cursor.x, screen.cursor.y), (4, 3))
         assert_startswith(lambda: screen.display[3], "r$>")
+
+    with screen_process(rtichoke_command + ["--version"]) as screen:
+        assert_startswith(lambda: screen.display[0], "rtichoke version: ")
+        import rtichoke
+        assert screen.display[0][18:].strip() == rtichoke.__version__
