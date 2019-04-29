7c7119521f4bc7e9204dac2309e42e4ea9fbdb85
Randy Lai <randy.cs.lai@gmail.com>
2019-04-29 11:20:51 -0400

    make sure key is a list

--- radian/R/key_bindings.R
+++ radian/R/key_bindings.R
@@ -5,7 +5,7 @@ radian <- import("radian")
 keymap <- getOption("radian.key_map")
 if (!is.null(keymap)) {
     for (map in keymap) {
-        key <- map$key
+        key <- as.list(map$key)
         value <- map$value
         mode <- if (is.null(map$mode)) "r" else map$mode
         radian$key_bindings$map_key(key, value, mode)
