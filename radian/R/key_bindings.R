e9e7c7333cd57216736e58185e809fb74c92bbea
Randy Lai <randy.cs.lai@gmail.com>
2019-05-01 00:53:10 -0400

    support mapping all keys (unsafe and not documented)

--- radian/R/key_bindings.R
+++ radian/R/key_bindings.R
@@ -2,10 +2,22 @@ getOption("rchitect.py_tools")$attach()
 
 radian <- import("radian")
 
-keymap <- getOption("radian.escape_key_map")
+esc_keymap <- getOption("radian.escape_key_map")
+if (!is.null(esc_keymap)) {
+    for (map in esc_keymap) {
+        key <- list("escape", map$key)
+        value <- map$value
+        mode <- if (is.null(map$mode)) "r" else map$mode
+        radian$key_bindings$map_key(key, value, mode)
+    }
+}
+
+
+# not documented
+keymap <- getOption("radian.key_map")
 if (!is.null(keymap)) {
     for (map in keymap) {
-        key <- list("escape", map$key)
+        key <- as.list(map$key)
         value <- map$value
         mode <- if (is.null(map$mode)) "r" else map$mode
         radian$key_bindings$map_key(key, value, mode)
