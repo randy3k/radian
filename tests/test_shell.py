d0a412ee680b95a9ff3c722f51a1695497f03c1c
Randy Lai <randy.cs.lai@gmail.com>
2019-05-07 22:20:50 -0400

    wait until radian starts

--- tests/test_shell.py
+++ tests/test_shell.py
@@ -4,6 +4,7 @@ from .utils import assert_startswith
 
 def test_shell(radian_terminal):
     screen = radian_terminal.screen
+    assert_startswith(lambda: screen.display[3], "r$>")
     radian_terminal.write(b";")
     assert_startswith(lambda: screen.display[3], "#!>")
     radian_terminal.write(b"python --version\n")
