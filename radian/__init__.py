32a36ebc920f4c93847619988d7d6e402fdb36dc
Randy Lai <randy.cs.lai@gmail.com>
2019-09-13 14:10:07 -0700

    allow -q flag

--- radian/__init__.py
+++ radian/__init__.py
@@ -12,7 +12,7 @@ def main():
     parser = optparse.OptionParser("usage: radian")
     parser.add_option("-v", "--version", action="store_true", dest="version", help="get version")
     parser.add_option("--profile", dest="profile", help="Path to .radian_profile")
-    parser.add_option("--quiet", action="store_true", dest="quiet", help="Don't print startup message")
+    parser.add_option("-q", "--quiet", action="store_true", dest="quiet", help="Don't print startup message")
     parser.add_option("--no-environ", action="store_true", dest="no_environ", help="Don't read the site and user environment files")
     parser.add_option("--no-site-file", action="store_true", dest="no_site_file", help="Don't read the site-wide Rprofile")
     parser.add_option("--no-init-file", action="store_true", dest="no_init_file", help="Don't read the user R profile")
