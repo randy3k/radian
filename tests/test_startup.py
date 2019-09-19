fdeb35c5bb5a4d8c739a7d4fe15707d4b96d94dc
Randy Lai <randy.cs.lai@gmail.com>
2019-09-18 17:41:19 -0700

    debug failed test

--- tests/test_startup.py
+++ tests/test_startup.py
@@ -2,12 +2,16 @@ from __future__ import unicode_literals
 
 
 def test_startup(terminal):
-    terminal.line(0).assert_startswith("R ")
-    terminal.cursor().assert_equal((4, 3))
-    terminal.current_line().assert_startswith("r$>")
-    terminal.write("\n")
-    terminal.current_line().assert_startswith("r$>")
-    terminal.cursor().assert_equal((4, 5))
-    terminal.sendintr()
-    terminal.current_line().assert_startswith("r$>")
-    terminal.cursor().assert_equal((4, 7))
+    try:
+        terminal.line(0).assert_startswith("R ")
+        terminal.cursor().assert_equal((4, 3))
+        terminal.current_line().assert_startswith("r$>")
+        terminal.write("\n")
+        terminal.current_line().assert_startswith("r$>")
+        terminal.cursor().assert_equal((4, 5))
+        terminal.sendintr()
+        terminal.current_line().assert_startswith("r$>")
+        terminal.cursor().assert_equal((4, 7))
+    except Exception:
+        print("\n".join(terminal.screen.display))
+        raise
