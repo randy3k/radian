1d05e8f16f8cce2b8ee77d7dd6707ab5adfdccbc
Randy Lai <randy.cs.lai@gmail.com>
2021-02-04 16:58:26 -0800

    set the default value of `emacs_bindings_in_vi_insert_mode` to False

--- radian/settings.py
+++ radian/settings.py
@@ -37,7 +37,7 @@ class RadianSettings(object):
 
     def load(self):
         self._load_setting("auto_suggest", False, bool)
-        self._load_setting("emacs_bindings_in_vi_insert_mode", True, bool)
+        self._load_setting("emacs_bindings_in_vi_insert_mode", False, bool)
         self._load_setting("editing_mode", "emacs")
         self._load_setting("color_scheme", "native")
         self._load_setting("auto_match", True, bool)
