e0cb56aee59532859b24932d7f51de7ea78bbaf8
Randy Lai <randy.cs.lai@gmail.com>
2021-12-10 13:11:09 -0800

    add silent flag

--- radian/__init__.py
+++ radian/__init__.py
@@ -20,7 +20,7 @@ def main():
     parser.add_option("-v", "--version", action="store_true", dest="version", help="Get version")
     parser.add_option("--r-binary", dest="r", help="Path to R binary")
     parser.add_option("--profile", dest="profile", help="Path to .radian_profile, ignore both global and local profiles")
-    parser.add_option("-q", "--quiet", action="store_true", dest="quiet", help="Don't print startup message")
+    parser.add_option("-q", "--quiet", "--silent", action="store_true", dest="quiet", help="Don't print startup message")
     parser.add_option("--no-environ", action="store_true", dest="no_environ", help="Don't read the site and user environment files")
     parser.add_option("--no-site-file", action="store_true", dest="no_site_file", help="Don't read the site-wide Rprofile")
     parser.add_option("--no-init-file", action="store_true", dest="no_init_file", help="Don't read the user R profile")
