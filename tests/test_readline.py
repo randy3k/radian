75c20454a841104f5b65003c26479d037b71b54f
Randy Lai <randy.cs.lai@gmail.com>
2021-12-16 22:49:19 -0800

    longer timeout for the first test

--- tests/test_readline.py
+++ tests/test_readline.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 def test_readline(terminal):
     # issue #106
-    terminal.current_line().assert_startswith("r$>")
+    terminal.current_line().assert_startswith("r$>", timeout=30)
     terminal.write("cat('hello'); readline('> ')\n")
     terminal.previous_line(1).assert_startswith("hello")
     terminal.current_line().assert_startswith("> ")
