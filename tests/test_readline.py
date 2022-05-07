12d393efd16662e0b497e298b0090566aa857255
Randy Lai <randy.cs.lai@gmail.com>
2022-05-06 17:14:16 -0700

    add a test for getpass

--- tests/test_readline.py
+++ tests/test_readline.py
@@ -4,3 +4,12 @@ def test_readline(terminal):
     terminal.write("cat('hello'); readline('> ')\n")
     terminal.previous_line(1).assert_startswith("hello")
     terminal.current_line().assert_startswith("> ")
+
+
+def test_askpass(terminal):
+    # issue #359
+    terminal.current_line().assert_startswith("r$>")
+    terminal.write("askpass::askpass('askpass> ')\n")
+    terminal.current_line().assert_startswith("askpass>")
+    terminal.write("answer\n")
+    terminal.previous_line(2).assert_contain("\"answer\"")
