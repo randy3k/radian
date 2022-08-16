628d417a76b1244d77751ebb781e967f1b40ebd2
Randy Lai <randy.cs.lai@gmail.com>
2022-08-16 10:56:20 -0700

    add string tests

--- tests/test_readline.py
+++ tests/test_readline.py
@@ -1,3 +1,6 @@
+import time
+
+
 def test_readline(terminal):
     # issue #106
     terminal.current_line().assert_startswith("r$>")
@@ -13,3 +16,32 @@ def test_askpass(terminal):
     terminal.current_line().assert_startswith("askpass>")
     terminal.write("answer\n")
     terminal.previous_line(2).assert_contain("\"answer\"")
+
+
+def test_strings(terminal):
+    # issue #377
+    terminal.current_line().assert_startswith("r$>")
+    terminal.write("x <- 'a'\n")
+    terminal.current_line().strip().assert_equal("r$>")
+    terminal.write("nchar(x)\n")
+    terminal.previous_line(2).assert_startswith("[1] 1")
+
+    terminal.write("\x1b[200~x <- '" + 'a'*10 + "'\x1b[201~\n")
+    terminal.current_line().strip().assert_equal("r$>")
+    terminal.write("nchar(x)\n")
+    terminal.previous_line(2).assert_startswith("[1] 10")
+
+    terminal.write("\x1b[200~x <- '" + 'a'*5000 + "'\x1b[201~\n")
+    terminal.current_line().strip().assert_equal("r$>")
+    terminal.write("nchar(x)\n")
+    terminal.previous_line(2).assert_startswith("[1] 5000")
+
+    terminal.write("\x1b[200~x <- '" + 'a'*2000 + '\n' + 'b'*2000 + "'\x1b[201~\n")
+    terminal.current_line().strip().assert_equal("r$>")
+    terminal.write("nchar(x)\n")
+    terminal.previous_line(2).assert_startswith("[1] 4001")
+
+    terminal.write("\x1b[200~x <- '" + '中'*1000 + '\n' + '文'*1000 + "'\x1b[201~\n")
+    terminal.current_line().strip().assert_equal("r$>")
+    terminal.write("nchar(x)\n")
+    terminal.previous_line(2).assert_startswith("[1] 2001")
